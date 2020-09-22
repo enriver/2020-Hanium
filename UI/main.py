550efb65285bf1da1b3131a7e29321d4c4fc1c5e
enriver <riverkeh@naver.com>
2020-09-22 22:49:39 +0900

    키움 API 등록

--- UI/main.py
+++ UI/main.py
@@ -5,33 +5,29 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QAxContainer import *
 
-class MyWindow(QMainWindow):
+class App(QWidget):
+    
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("존나하기싫다")
-        self.setGeometry(300,300,300,400)
+        self.initUI()
 
-        self.kiwoom=QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+    def initUI(self):
+        self.setWindowTitle('Hello Bryan')
+        self.center()
+        self.resize(500, 200)   # 창 크기
+        self.show()
 
-        btn1=QPushButton("Login",self)
-        btn1.move(20,20)
-        btn1.clicked.connect(self.btn1_clicked)
+    '''
+    화면의 가운데로 띄우기
+    '''
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
-        btn2=QPushButton("Check State",self)
-        btn2.move(20,70)
-        btn2.clicked.connect(self.btn2_clicked)
 
-    def btn1_clicked(self):
-        ret=self.kiwoom.dynamicCall("CommConnect()")
-
-    def btn2_clicked(self):
-        if self.kiwoom.dynamicCall("GetConnected()")==0:
-            self.statusBar().showMessage("Not connected")
-        else:
-            self.statusBar().showMessage("Connected")
-
-if __name__=="__main__":
-    app=QApplication(sys.argv)
-    mywindow=MyWindow()
-    mywindow.show()
-    app.exec_()
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = App()
+   sys.exit(app.exec_())
